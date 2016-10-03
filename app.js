e71f92cd999fd9ea9db53687b2270eae0d7b4135
Rich Pressler <richard.pressler@mesh01.com>
2016-10-02 20:44:02 -0400

    Fix port bug

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.put('/api/:table', api.update);
 
 app.get('/*', routes.indexHandler);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
     var host = server.address().address;
     var port = server.address().port;
